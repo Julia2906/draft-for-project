77ba9cf01dea3be6035a8a8de4917e966831997d
Julia2906 <137899526+Julia2906@users.noreply.github.com>
2025-02-06 21:14:31 +0200

    draft 06/02/2025 21:14

--- src/main.js
+++ src/main.js
@@ -1,21 +1,37 @@
 import Swiper from 'swiper';
-import { Navigation, Keyboard, Scrollbar } from 'swiper/modules';
+import { Navigation, Keyboard } from 'swiper/modules';
 
 const swiper = new Swiper('.swiper', {
-  modules: [Navigation, Keyboard, Scrollbar], // Вказуємо потрібні модулі
-  slidesPerView: 1,
-  spaceBetween: 32,
-  loop: false, // Вимикаємо нескінченну прокрутку
+  modules: [Navigation, Keyboard],
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: '.projects-swiper-button-next',
+    prevEl: '.projects-swiper-button-prev',
   },
+  spaceBetween: 32,
+  slidesPerView: 'auto',
   keyboard: {
     enabled: true,
-    onlyInViewport: true,
+    pageUpDown: true,
   },
-  scrollbar: {
-    el: '.swiper-scrollbar',
-    draggable: true,
-  },
+  loop: false,
 });
+
+document.addEventListener('keydown', function (event) {
+  if (event.key === 'Tab') {
+    event.preventDefault();
+    if (event.shiftKey) {
+      swiper.slidePrev();
+    } else {
+      swiper.slideNext();
+    }
+  }
+});
+
+document
+  .querySelector('.projects-button')
+  .addEventListener('click', function () {
+    window.open(
+      'https://github.com/graudu12/portfolio-js-goit-team10',
+      '_blank'
+    );
+  });
